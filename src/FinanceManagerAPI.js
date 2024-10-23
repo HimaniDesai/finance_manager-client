81568d12cede4e193ad876589905dfa1445aae5d
himanidesai <de99hi@gmail.com>
2024-10-23 19:07:37 -0400

    Adding Budget Planning Dashboard compnent

--- src/FinanceManagerAPI.js
+++ src/FinanceManagerAPI.js
@@ -2,7 +2,9 @@ const api = `http://localhost:8080`
 const FinanceManagerAPI = {
     // API URL STRINGS FOR CALL USING AXIOS
     getExpenseForGivenMonth: (user_id, month, year) => api+ `/expenses/` + `${user_id}` + `/` + `${month}` + `/` + `${year}`,
-    getIncomeForGivenMonth: (user_id, month, year) => api+ `/incomes/` + `${user_id}` + `/` + `${month}` + `/` + `${year}`
+    getIncomeForGivenMonth: (user_id, month, year) => api+ `/incomes/` + `${user_id}` + `/` + `${month}` + `/` + `${year}`,
+    getExpenseForGivenYear: (user_id, year) => api+ `/expenses/` + `${user_id}` + `/` + `${year}`,
+    getIncomeForGivenYear: (user_id, year) => api+ `/incomes/` + `${user_id}` + `/` + `${year}`,
 }
 
 export default FinanceManagerAPI;
